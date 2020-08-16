f240dc49bdb915a66df2e489e15d7fdc214cb14e
dileep <ravuladileep98@gmail.com>
2020-08-16 11:26:04 +0530

    changes in service

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -10,6 +10,13 @@ export class AppService {
 
   constructor(private http: HttpClient) { }
 
+  /**
+   * function: getPosts
+   * purpose: used to get the posts
+   * version: 1.0.1
+   * author : dileep_ravula
+   */
+
   getPosts(): Observable<any>{
    return this.http.get(`${environment.apiUrl}posts`);
   }
